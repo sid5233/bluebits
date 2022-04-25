5e72226267a9e0c390d00307298630f4bc7522d1
sid5233 <sudarshan.bhamare@gmail.com>
2022-04-25 13:19:00 +0530

    basic setup with two apps admin and ngshop

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngshop-root',
+  selector: 'bluebits-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngshop';
+  title = 'admin';
 }
---
+++ apps/ngshop/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngshop-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+})
+export class HomePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
